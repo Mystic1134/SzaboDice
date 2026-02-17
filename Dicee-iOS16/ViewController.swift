f84ffe2f8c9f1243566c2c350f1d2542d2803222
Mystic1134 <szabolaszlo328@gmail.com>
2026-02-16 21:42:03 -0500

    Dicee app before improvements

--- Dicee-iOS16/ViewController.swift
+++ Dicee-iOS16/ViewController.swift
@@ -1,20 +1,25 @@
 //
 //  ViewController.swift
-//  Dicee-iOS13
+//  Dicee-iOS16
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Laszlo Szabo on 2/15/2026
+//  My version of the Dice app with add ons
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
-
